13375154e2662068fb5dad12e23a59eb3b557977
Cody Moss <Cody Moss@CODYMOSSCOMP>
2016-10-22 11:09:41 -0600

    Add project files.

---
+++ OOPDemo/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDemo
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        protected double vacationDays;
+        public virtual void TakeVacation() { }
+        public Employee(string name)
+        {
+            Name = name; 
+        }
+        public override string ToString()
+        {
+            return $"[Employee: NAme = {Name}]";
+        }
+    }
+}
---
+++ OOPDemo/Manager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDemo
+{
+    class Manager : Employee
+    {
+        public bool CompanyCar { get; set; }
+
+        public Manager(string name, bool hasCar)
+            :base(name)
+        {
+            CompanyCar = hasCar;
+        }
+
+        public override void TakeVacation()
+        {
+            vacationDays += 15;
+        }
+
+        public void DriveCar()
+        {
+            if (CompanyCar)
+            {
+                Console.WriteLine("Driving the Car");
+            }
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
